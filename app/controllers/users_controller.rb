20112c55841564333bea6ee949e6268104046c94
Vitaly Zenin <originpolaroid@gmail.com>
2020-03-19 02:01:29 +0300

    users roots and basic views added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,9 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   
   def index
+    @users = User.all.order(id: :asc)
+  end
+
+  def show
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'welcome/index'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:index, :show]
 
   root to: "welcome#index"
 end
