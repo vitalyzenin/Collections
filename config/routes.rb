2177ecaf17a8822f23262dd69a00dca1697fdaed
Vitaly Zenin <originpolaroid@gmail.com>
2020-03-19 01:05:44 +0300

    welcome controller returned as home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: "users#index"
+  root to: "welcome#index"
 end
