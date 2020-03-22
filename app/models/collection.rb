1204c041c3fec7bc9fb4ff605cc3b4d1cf62eae5
Vitaly Zenin <originpolaroid@gmail.com>
2020-03-22 03:45:50 +0300

    different users now can have the same collection name

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,4 +1,4 @@
 class Collection < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :name, presence: true, uniqueness: { scope: :user_id }, length: { minimum: 3 }
 end
