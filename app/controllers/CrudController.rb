80517872f12f64a9e183bcfe956eafd647535a27
Vitaly Zenin <originpolaroid@gmail.com>
2020-03-30 17:31:48 +0300

    blocking users is taking effect now

--- app/controllers/CrudController.rb
+++ app/controllers/CrudController.rb
@@ -1,3 +1,13 @@
 class CrudController < ApplicationController
   before_action :authenticate_user!, except: [ :index, :show ]
+  before_action :block_bad_users!, except: [ :index, :show ]
+
+  private
+  def block_bad_users!
+    if current_user.blocked
+      flash[:notice] = 'You are banned!'
+      redirect_to root_path
+      return false
+    end
+  end
 end
