42875a7b5f6581baaa5ecf97b845464ef7c4a1ed
Vitaly Zenin <originpolaroid@gmail.com>
2020-04-04 18:23:49 +0300

    decreasing verbosity in code

--- app/controllers/privileges_controller.rb
+++ app/controllers/privileges_controller.rb
@@ -2,11 +2,7 @@ class PrivilegesController < AdminController
 
   def update
     user = User.find(params[:user_id])
-    if (user.admin == true)
-      user.update(admin: false)
-    else
-      user.update(admin: true)
-    end
+    user.update(admin: !user.admin)
     redirect_to users_path
   end
 end
